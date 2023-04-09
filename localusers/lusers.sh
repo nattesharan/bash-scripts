128c56ad6c9cb40c00b439346929808d26563f50
nattesharan <nattesharan@gmail.com>
2023-04-09 16:06:08 +0530

    Update lusers.sh
    combining and reassigning variables

--- localusers/lusers.sh
+++ localusers/lusers.sh
@@ -7,3 +7,12 @@ MESSAGE="Hello World is working"
 echo "Message is: $MESSAGE"
 # appending text to variable
 echo "${MESSAGE}is text"
+
+# combining variables
+MESSAGE2=" fine"
+echo "${MESSAGE}${MESSAGE2}"
+# reassigning variables
+MESSAGE="Hello world...."
+echo $MESSAGE
+echo "$MESSAGE happy world"
+echo "${MESSAGE}${MESSAGE2}"
